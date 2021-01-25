13745d80e4f7e449e854865dde2c6c6aaf0685d1
Courtney Tern <catern19@gmail.com>
2021-01-25 11:46:49 -0500

    complete revamp

--- _posts/2020-07-01-firstpost.md
+++
@@ -1,7 +0,0 @@
----
-type: posts
----
-## First Blog post
-
-This is my first blog post!
-07.01.2020
--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Firstpost",
-        "excerpt":"First Blog post   This is my first blog post! 07.01.2020  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/firstpost/",
-        "teaser": null
-      }]
